5f8e5fc1af7e14c0a1bccc8d3229cff156a4446e
Tycho Bokdam <t.bokdam@seronaris.nl>
2017-10-15 18:57:04 +0200

    Small update in the generating of breakpoints

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,60 +1,61 @@
-// @flow weak
+// @flow
 
-export type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+export type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
 // Sorted ASC by size. That's important.
+// It can't be configured as it's used statically for propTypes.
 export const keys = ['xs', 'sm', 'md', 'lg', 'xl']
 
-export const defaultBreakpoints = {
-  xs: 360,
-  sm: 600,
-  md: 960,
-  lg: 1280,
-  xl: 1920,
-}
-
 // Keep in mind that @media is inclusive by the CSS specification.
-export default (breakpoints = defaultBreakpoints, unit = 'px', step = 1) => {
-  const values = keys.map(key => breakpoints[key])
+export default function createBreakpoints (breakpoints: Object = {}) {
+  const {
+          values = {
+            xs: 360,
+            sm: 600,
+            md: 960,
+            lg: 1280,
+            xl: 1920,
+          },
+          unit   = 'px',
+          step   = 1,
+          ...other
+        } = breakpoints
 
-  const up = (key) => {
+  function up (key) {
     let value
     // min-width of xs starts at 0
     if (key === 'xs') {
       value = 0
     } else {
-      value = breakpoints[key] || key
+      value = values[key] || key
     }
-
     return `@media (min-width:${value}${unit})`
   }
 
-  const down = (key) => {
-    const value = breakpoints[key] || key
+  function down (key) {
+    const value = values[key] || key
     return `@media (max-width:${value - step / 100}${unit})`
   }
 
-  const between = (start, end) => {
+  function between (start, end) {
     const startIndex = keys.indexOf(start)
     const endIndex   = keys.indexOf(end)
-
     return (
-      `@media (min-width:${values[startIndex]}${unit}) and ` +
-      `(max-width:${values[endIndex + 1] - step / 100}${unit})`
+      `@media (min-width:${values[keys[startIndex]]}${unit}) and ` +
+      `(max-width:${values[keys[endIndex + 1]] - step / 100}${unit})`
     )
   }
 
-  const only = (key) => {
+  function only (key) {
     const keyIndex = keys.indexOf(key)
     if (keyIndex === keys.length - 1) {
       return up(key)
     }
-
     return between(key, key)
   }
 
-  const getWidth = (key) => {
-    return breakpoints[key]
+  function width (key) {
+    return values[key]
   }
 
   return {
@@ -64,6 +65,7 @@ export default (breakpoints = defaultBreakpoints, unit = 'px', step = 1) => {
     down,
     between,
     only,
-    getWidth,
+    width,
+    ...other,
   }
 }
